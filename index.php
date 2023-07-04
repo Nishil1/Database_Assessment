0934382676be176d8a644d0ba85b6fc9063193d4
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-07-04 12:06:38 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+	include "head_nav.php";
+
+?>
+
+
+        
+        <div class="box main">
+            <h2>Welcome</h2>
+            
+            <p>
+				This website was made to show you around various types of dishes of my favroite dishes and get my opinion and ratings on them.
+            </p>
+            
+            <p>
+				I hope you will enjoy reading them and as usual mroe will be added as time goes on. Feel free to message me via the contact form to speak with me.
+            </p>
+            
+            
+        </div>    <!-- / main -->
+        
+<?php 
+	include "bottombit.php";
+
+?>
+ 
