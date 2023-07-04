9ec0d5f24a12d9a4c2031d3c98f2ac6f2b0978ab
Nishil1 <100240793+Nishil1@users.noreply.github.com>
2023-07-04 12:05:55 +1200

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define ("DB_NAME", "bharadwajn1136_L1_Books_Practice");
+define ("DB_USERNAME", "_bharadwajn1136");
+define ("DB_PASSWORD", "yXkv2tMicU0xUGy8");
+define ("DB_HOST", "localhost");
+
+?>
